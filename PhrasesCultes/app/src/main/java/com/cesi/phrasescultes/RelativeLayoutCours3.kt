e279b8aa010fc6928755c313b79d0f502e811349
Dima <dima@dalloul.ch>
2021-02-03 11:49:10 +0100

    Cours 3 - Slide 13 - Correction

---
+++ PhrasesCultes/app/src/main/java/com/cesi/phrasescultes/RelativeLayoutCours3.kt
@@ -0,0 +1,12 @@
+package com.cesi.phrasescultes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RelativeLayoutCours3: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.relativelayout_cours3)
+    }
+}
